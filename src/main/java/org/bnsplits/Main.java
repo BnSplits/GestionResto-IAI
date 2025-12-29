53ace10cf57b74d81563a5f567e883c63f337e58
BnSplits <bnsp.x0@gmail.com>
2025-12-29 09:53:54 +0000

    Création des dossiers controller, model, view

--- src/main/java/org/bnsplits/Main.java
+++ src/main/java/org/bnsplits/Main.java
@@ -1,12 +1,21 @@
 package org.bnsplits;
 
 import com.formdev.flatlaf.FlatDarkLaf;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+
         // Appliquer le thème sombre de FlatLaf
         FlatDarkLaf.setup();
         System.out.println("Hello World");
+
+        JFrame frame = new JFrame("Gestion Restoration");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1280, 720);
+        frame.setVisible(true);
     }
 }
